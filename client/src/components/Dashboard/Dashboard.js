8bee27957c284162703f0aca177e6e9dca7369ea
Ayush Jha <ayushkumarjha666@gmil.com>
2024-02-21 20:53:07 +0530

    testing

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import  {React, useEffect, useState } from 'react';
+import {axios} from 'axios';
 import './Dashboard.css'
 
 const Dashboard = () => {
@@ -10,9 +10,9 @@ const Dashboard = () => {
             try {
                 const response = await axios.get('http://localhost:8000/api/dashboard');
                 // console.log(response);
-                setUsers(response.data);
+                setUsers(response?.data);
             } catch (error) {
-                console.error('Error fetching users:', error.message);
+                console.error('Error fetching users:', error?.message);
             }
         };
 
@@ -20,18 +20,18 @@ const Dashboard = () => {
     }, []);
 
     return (
-        <div className='stD'>
+        <div className='stD' data-testid="dash-test">
             <h1 className='dashHH'>Dashboard</h1>
             <div className='ndD'>
                 {/* <h2>All Users</h2> */}
                 <div className='rdD'>
-                    {users.map(user => (
-                        <div className='allD' key={user._id}>
+                    {users?.map(user => (
+                        <div className='allD' key={user?._id}>
                             <div className='userDetails'>
-                                <div className='detail'>Name: {user.fullName}</div>
-                                <div className='detail'>Logged In At : {user.loggedInTime ? new Date(user.loggedInTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'LogOut...'}</div>
-                                <div className='detail'>Logged Out At : {user.loggedOutTime ? new Date(user.loggedOutTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'Login...'}</div>
-                                <div className='detail'>Date : {user.loggedInTime ? new Date(user.loggedInTime).toLocaleDateString([], { month: 'short', day: '2-digit' }) : 'LogOut...'}</div>
+                                <div className='detail'>Name: {user?.fullName}</div>
+                                <div className='detail'>Logged In At : {user?.loggedInTime ? new Date(user.loggedInTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'LogOut...'}</div>
+                                <div className='detail'>Logged Out At : {user?.loggedOutTime ? new Date(user.loggedOutTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'Login...'}</div>
+                                <div className='detail'>Date : {user?.loggedInTime ? new Date(user.loggedInTime).toLocaleDateString([], { month: 'short', day: '2-digit' }) : 'LogOut...'}</div>
                             </div>
                         </div>
                     ))}
---
+++ client/src/components/__tests__/dashboard.test.jsx
@@ -0,0 +1,25 @@
+import Dashboard from '../Dashboard/Dashboard.js'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+
+test('should render dashboard component', () => {
+
+    render(
+        <Dashboard />
+    );
+
+    const dashElement = screen.getByTestId('dash-test');
+    expect(dashElement).toBeInTheDocument();
+    expect(dashElement).toContainHTML('</h1>');
+});
+
+test('match snapsot', () => {
+
+    const tree = renderer.create(
+        <Dashboard />
+    ).toJSON();
+
+    console.log(tree);
+    expect(tree).toMatchSnapshot();
+});
